4bca00d2a989814f12d1c87b09e247245ba92119
Adarsh Singh <Adarsh.Singh@watchguard.com>
2024-10-18 06:05:06 +0530

    this is the correct and working version of the previous comit

--- utils/connection.py
+++ utils/connection.py
@@ -1,4 +1,4 @@
 import sqlite3
-# conn = sqlite3.connect('database.sqlite')
-conn = sqlite3.connect('databasee.sqlite')
+conn = sqlite3.connect('database.sqlite')
+# conn = sqlite3.connect('databasee.sqlite')
 cursor = conn.cursor()
